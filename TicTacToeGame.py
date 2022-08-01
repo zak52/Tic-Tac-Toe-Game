dfe3d34cd4f31d1bd412d6bcb5627eeacd0f72f0
zchkfmn <77305557+zchkfmn@users.noreply.github.com>
2022-07-31 17:00:39 -0700

    Update TicTacToeGame.py
    Adding title Screen

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -4,6 +4,7 @@
 import random, pygame, sys
 from pygame.locals import *
 
+FPS = 15
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
 CELLSIZE = 20
@@ -21,7 +22,7 @@ GREEN     = (  0, 255,   0)
 DARKGREEN = (  0, 155,   0)
 DARKGRAY  = ( 40,  40,  40)
 BLUE      = (  0,   0, 255)
-BGCOLOR = BLACK
+LIGHTGRAY = (192, 192, 192)
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT # global variables for screen size
@@ -32,16 +33,16 @@ def main():
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     pygame.display.set_caption('TicTacToe')
 
-    setUpGameScreen()
-    # showStartScreen()
+    showStartScreen()
     while True: # main games loop
+        setUpGameScreen()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
 
-
+# Sets up the game screen for the game
 def setUpGameScreen():
     DISPLAYSURF.fill(WHITE)
     pygame.draw.line(DISPLAYSURF, BLACK, (400, 40), (400, 440), 4)
@@ -51,4 +52,56 @@ def setUpGameScreen():
 
     # pixObj = pygame.PixelArray(DISPLAYSURF)
 
+
+def drawPressKeyMsg():
+    pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
+    pressKeyRect = pressKeySurf.get_rect()
+    pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
+    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+def checkForKeyPress():
+    if len(pygame.event.get(QUIT)) > 0:
+          terminate()
+
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
+
+def showStartScreen():
+    titleFont = pygame.font.Font("freesansbold.ttf", 100)
+    titleSurf1 = titleFont.render("TicTacToe!", True, DARKGRAY, RED)
+    titleSurf2 = titleFont.render("TicTacToe!", True, LIGHTGRAY)
+
+    degrees1 = 0
+    degrees2 = 0
+
+    while True:
+        DISPLAYSURF.fill(WHITE)
+        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
+        rotatedRect1 = rotatedSurf1.get_rect()
+        rotatedRect1.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
+
+        rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
+        rotatedRect2 = rotatedSurf2.get_rect()
+        rotatedRect2.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
+
+        drawPressKeyMsg()
+
+        if checkForKeyPress():
+            pygame.event.get()
+            return
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+        degrees1 += 3
+        degrees2 += 7
+                
+def terminate():
+    pygame.quit()
+    sys.exit()
+                                  
 main()
